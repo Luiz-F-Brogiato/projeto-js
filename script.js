ea3e76f05c9510dcf9c51f01e47a3b69c9216c7c
Luiz-F-Brogiato <brogiatoluizfernando@gmail.com>
2025-04-16 20:05:44 -0300

    Array

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+var numero1 = 1;
+var numero2 = "1";
+
+if (numero1 === numero2) {
+    console.log("Igual");
+} else {
+    console.log("Diferente")
+}
+ */
+
+
+
+var nome = "Luiz Fernando";
